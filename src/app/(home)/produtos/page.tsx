6e7e48f111a1f9f5c5eb6af70f19675857b4af87
Thiago <thiagosouza2902@gmail.com>
2024-09-10 01:47:15 -0300

    still trying 2

--- src/app/(home)/produtos/page.tsx
+++ src/app/(home)/produtos/page.tsx
@@ -5,6 +5,7 @@ import HeroSection from "@/components/home/herosection";
 import Products from "@/components/home/Products";
 import Cards from "@/components/home/Products/cards";
 import { Pegaproduto } from "@/back/home/actions";
+import { Suspense } from "react";
 
 export default async function Home() {
   const produtos = await Pegaproduto()
@@ -12,7 +13,9 @@ export default async function Home() {
     <main className="bg-bege">
       <div className="flex justify-center flex-col items-center gap-12 py-12">
         <div className="flex flex-wrap gap-8 justify-center w-3/4">
+          <Suspense>
           <Products produtos={produtos} title={"Todos os Produtos"} />
+          </Suspense>
           {/*<Cards image="/image/aurora.png" title="Aurora - All my demons greeting me as a friend" price="R$ 79,00" />*/}
         </div>
       </div>
