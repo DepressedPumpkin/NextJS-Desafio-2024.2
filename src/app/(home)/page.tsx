60b19b1af9e5347260377729e067db0c5bd57093
Thiago <thiagosouza2902@gmail.com>
2024-09-10 01:52:05 -0300

    suspense in other page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -8,13 +8,16 @@ import Edit from "@/components/crud/editar";
 import Delete from "@/components/crud/delete";
 import Criar from "@/components/crud/criar";
 import { Pega4produto } from "@/back/home/actions";
+import { Suspense } from "react";
 
 export default async function Home() {
   const produtos = await Pega4produto()
   return (
     <main className="bg-bege">
     <HeroSection/>
+    <Suspense> 
     <Products produtos={produtos} title={"Principais Produtos"}/>
+    </Suspense>
     <Missao/>
     </main>
   );
